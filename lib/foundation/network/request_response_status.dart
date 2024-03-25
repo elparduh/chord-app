63ad9aacf0d0dc8f0684bbedd0f9d58f5b8f8c9d
Juan Ticante Vicente <juan.ticante@justo.mx>
2024-03-24 21:24:50 -0600

    add improvs

--- lib/foundation/network/request_response_status.dart
+++ lib/foundation/network/request_response_status.dart
@@ -1,31 +1,31 @@
 import 'network_constants.dart';
 
-sealed class RequestStatus {
-  const RequestStatus(this.message);
+sealed class RequestResponseStatus {
+  const RequestResponseStatus(this.message);
   final String message;
 }
 
-class SuccessfulRequest extends RequestStatus {
+class SuccessfulRequest extends RequestResponseStatus {
   const SuccessfulRequest() : super(NetworkConstants.successfulRequestMessage);
 }
 
-class AccessDenied extends RequestStatus {
+class AccessDenied extends RequestResponseStatus {
   const AccessDenied() : super(NetworkConstants.accessDeniedMessage);
 }
 
-class UnavailableServer extends RequestStatus {
+class UnavailableServer extends RequestResponseStatus {
   const UnavailableServer() : super(NetworkConstants.unavailableServerMessage);
 }
 
-class ConnectionNetwork extends RequestStatus {
+class ConnectionNetwork extends RequestResponseStatus {
   const ConnectionNetwork() : super(NetworkConstants.connectionNetworkMessage);
 }
 
-class Unknown extends RequestStatus {
+class Unknown extends RequestResponseStatus {
   const Unknown(this.errorMessage) : super(errorMessage);
   final String errorMessage;
 }
 
-extension on RequestStatus {
+extension on RequestResponseStatus {
   hasErrors() => this is! SuccessfulRequest;
 }

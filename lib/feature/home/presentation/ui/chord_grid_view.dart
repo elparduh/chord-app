09defef42c19cbd7a4b28df998470e9e22e805db
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-05-01 21:40:14 -0600

    add improvs

--- lib/feature/home/presentation/ui/chord_grid_view.dart
+++ lib/feature/home/presentation/ui/chord_grid_view.dart
@@ -5,51 +5,42 @@ import 'package:mrchord_app/feature/home/presentation/bloc/chord_state.dart';
 import 'package:mrchord_app/feature/home/presentation/ui/tile_chord_view.dart';
 
 class ChordGridView extends StatelessWidget {
-  const ChordGridView({Key? key}): super(key: key);
+  const ChordGridView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder <ChordBloc, ChordState>(
-        builder: (context, state) {
-          final createChordStateStatus = state.createChordStateStatus;
-          if (createChordStateStatus is CreateChordStateLoading) {
-            return Container(
-                padding: const EdgeInsets.all( 16.0,
-                ),
-                child: const Center(
-                  child: CircularProgressIndicator(),
-                ));
-          }
-
-          if (createChordStateStatus is CreateChordStateSuccess) {
-            final chordList = createChordStateStatus.chordUiList;
-            return GridView.builder(
-                itemCount: chordList.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2
-                ),
-                itemBuilder: (context, index) {
-                  return TileChordView(
-                    chordList[index]
-                  );
-                }
-            );
-          }
-
-          return Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(
-              vertical: 32.0,
-              horizontal: 16.0
+    return BlocBuilder<ChordBloc, ChordState>(builder: (context, state) {
+      final createChordStateStatus = state.createChordStateStatus;
+      if (createChordStateStatus is CreateChordStateLoading) {
+        return Container(
+            padding: const EdgeInsets.all(
+              16.0,
             ),
-            child: const Text(
-              "Lista vacía",
-              style: TextStyle(
-                  fontSize: 16.0, fontWeight: FontWeight.normal),
-            ),
-          );
-        });
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ));
+      }
+
+      if (createChordStateStatus is CreateChordStateSuccess) {
+        final chordList = createChordStateStatus.chordUiList;
+        return GridView.builder(
+            itemCount: chordList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              return TileChordView(chordList[index]);
+            });
+      }
+
+      return Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+        child: const Text(
+          "Lista vacía",
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal),
+        ),
+      );
+    });
   }
 }
-
